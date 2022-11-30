08584cdde68f3b2196c8f710bb62fd9b5b7a80c4
Tolgay Altiner <tolgay.altiner@web.de>
2022-11-30 17:07:55 +0100

    path to css fixed

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const { appendFile } = require('fs');
 
 const app = express();
 app.engine('ejs', ejsMate);
+app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
