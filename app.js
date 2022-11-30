4a071e49b2cc851f060204224e9f778d9504c79f
TimHa91 <45972852+timHa91@users.noreply.github.com>
2022-11-30 15:36:24 +0100

    Initial Project Structure

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const { appendFile } = require('fs');
+
+// mongoose.connect('mongodb://localhost:27018/veggie-connect', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
+const app = express();
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
+});
